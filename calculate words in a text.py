1df12d3ec309e9a06928a96cd25057a41c9164a1
Ufurus <slavkata72@gmail.com>
2025-09-18 08:51:47 +0300

    Update calculate words in a text.py

--- calculate words in a text.py
+++ calculate words in a text.py
@@ -6,9 +6,13 @@ entered_text = input("Enter text: ")
 counter = 0
 word_counter = 0
 
-if len(entered_text) == 0:
+if len(entered_text) == 0: # check if no words are provided
 
     print("No words provided!")
+
+# Below checks if string is more than 0 in terms of length, but whitespaces are also considered as length.
+# As so, if perhaps the space-bar was hit several times it will add those whitespaces to the string, thus making it
+# 0 < which we need to check if this length is whitespace or not.
 
 if len(entered_text) > 0:
 
